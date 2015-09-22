1285a40536098b24f8d180bb549369b5926fe59d
Eric <eacajolet@quinnipiac.edu>
2015-09-22 17:29:04 +0000

    Add questions to admin panel

--- core/models.py
+++ core/models.py
@@ -2,6 +2,9 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-  title = models.CharField(max_length=300)
-  description = models.TextField(null=True, blank=True)
-  created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=300)
+    description = models.TextField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+  
+    def __unicode__(self):
+      return self.title
